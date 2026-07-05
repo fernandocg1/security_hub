36c571d94db2593930bab66ad22f66896664eb57
Fernando Castro Gadelha <167661848+fernandocg1@users.noreply.github.com>
2026-07-04 22:28:26 -0300

    Corrigindo a formatação

--- teste_esp32.py
+++ teste_esp32.py
@@ -1,15 +1,14 @@
 import requests
 
 url = 'http://127.0.0.1:8000/api/events/'
+]
 
-# Aqui é onde o ESP32 apresenta o crachá VIP!
 headers = {
     'Authorization': 'Token 6513c2044c6d16cd5a459dda6c4dd3bd3b4f9388' 
 }
 
-# Dados falsos simulando a câmera YOLO detectando uma pessoa
 dados = {
-    "device": 1, # O ID da sua Câmera 
+    "device": 1,
     "event_type": "Pessoa Detectada no Quintal",
     "is_emergency": True
 }
